64ec333e5e7d4b6eddad76364b6c1e86c55ae517
PNone <urikita8@gmail.com>
2024-12-21 18:10:07 +0200

    Fix timeout env var

--- HomeworkTools/HW1/compile_and_test.sh
+++ HomeworkTools/HW1/compile_and_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LOCAL_GRADESCOPE_TIMEOUT 10
+export LOCAL_GRADESCOPE_TIMEOUT=10
 git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++11 -Wall -o test.out *.cpp
 python3 ./MatamGenericTester/run_tests.py ./TechnionDataStructures/HW1/tests/custom_tests.json ./test.out
