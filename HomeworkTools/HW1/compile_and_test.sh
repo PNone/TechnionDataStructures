e502e3ea181a2e622c58057c739c3c9ad8f92f6f
PNone <urikita8@gmail.com>
2024-12-22 17:37:53 +0200

    Forgiving timeouts HW1

--- HomeworkTools/HW1/compile_and_test.sh
+++ HomeworkTools/HW1/compile_and_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export LOCAL_GRADESCOPE_TIMEOUT=30
+export LOCAL_GRADESCOPE_VALGRIND_TIMEOUT=40
 git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++11 -Wall -o test.out *.cpp
 python3 ./MatamGenericTester/run_tests.py ./TechnionDataStructures/HW1/tests/custom_tests.json ./test.out
