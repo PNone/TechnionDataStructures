a8acd8cb044cd565eb341dc74d3b222fba8fd9f2
PNone <urikita8@gmail.com>
2024-12-24 17:50:33 +0200

    Stricted Timeouts

--- HomeworkTools/HW1/compile_and_test.sh
+++ HomeworkTools/HW1/compile_and_test.sh
@@ -2,8 +2,8 @@
 
 export LOCAL_GRADESCOPE_TIMEOUT=50
 export LOCAL_GRADESCOPE_VALGRIND_TIMEOUT=60
-export MATAM_TESTER_TEST_TIMEOUT=50
-export MATAM_TESTER_VALGRIND_TIMEOUT=60
+export MATAM_TESTER_TEST_TIMEOUT=25
+export MATAM_TESTER_VALGRIND_TIMEOUT=25
 git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++11 -Wall -o test.out *.cpp
 python3 ./MatamGenericTester/run_tests.py ./TechnionDataStructures/HW1/tests/custom_tests.json ./test.out
--- HomeworkTools/HW1/compile_and_test_mt.sh
+++ HomeworkTools/HW1/compile_and_test_mt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export MATAM_TESTER_TEST_TIMEOUT=50
-export MATAM_TESTER_VALGRIND_TIMEOUT=60
+export MATAM_TESTER_TEST_TIMEOUT=25
+export MATAM_TESTER_VALGRIND_TIMEOUT=25
 export MATAM_TESTER_RUN_MULTI_THREADED=1
 git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++11 -Wall -o test.out *.cpp
