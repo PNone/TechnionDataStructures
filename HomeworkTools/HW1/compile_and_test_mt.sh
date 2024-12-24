323ba40e6521727b8ac9532c297478ee35c6032c
PNone <urikita8@gmail.com>
2024-12-24 16:29:12 +0200

    Prepare for tester refactor

--- HomeworkTools/HW1/compile_and_test_mt.sh
+++ HomeworkTools/HW1/compile_and_test_mt.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-export LOCAL_GRADESCOPE_TIMEOUT=50
-export LOCAL_GRADESCOPE_VALGRIND_TIMEOUT=60
 export MATAM_TESTER_TEST_TIMEOUT=50
 export MATAM_TESTER_VALGRIND_TIMEOUT=60
+export MATAM_TESTER_RUN_MULTI_THREADED=1
 git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++11 -Wall -o test.out *.cpp
 python3 ./MatamGenericTester/run_tests.py ./TechnionDataStructures/HW1/tests/custom_tests.json ./test.out
