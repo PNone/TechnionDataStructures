c03b0980c22d4e7c2968a3436cb7f54389c12eb1
PNone <urikita8@gmail.com>
2025-01-22 18:16:22 +0200

    Add temp report support

--- HomeworkTools/HW2/compile_and_test.sh
+++ HomeworkTools/HW2/compile_and_test.sh
@@ -2,6 +2,7 @@
 
 export MATAM_TESTER_TEST_TIMEOUT=25
 export MATAM_TESTER_VALGRIND_TIMEOUT=25
+export MATAM_TESTER_EXPORT_TEMP_REPORT=1
 git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++11 -Wall -o test.out *.cpp
 python3 ./MatamGenericTester/run_tests.py ./TechnionDataStructures/HW2/tests/custom_tests.json ./test.out
