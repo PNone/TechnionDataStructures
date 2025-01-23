eaf967fcd9d9c2255ec2d3cd65e564532dbf5618
PNone <urikita8@gmail.com>
2025-01-23 15:11:59 +0200

    Remove unused stuff

--- HomeworkTools/HW5/compile_and_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git submodule update --init --recursive --remote
-export MATAM_TESTER_TRIMR_SPACES=1
-export MATAM_TESTER_IGNORE_EMPTY_LINES=1
-python3 ./MatamGenericTester/run_tests.py ./TechnionMatam/HW5/tests/custom_tests.json /usr/bin/python3 ./enigma.py
